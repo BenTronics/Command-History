11d5d8378a8e3422b3088ec5755ef4685449d1cc
Benni <>
2022-03-23 15:26:53 +0100

    methode add_item hat test bestanden

--- src/cmd_history.py
+++ src/cmd_history.py
@@ -1,18 +1,27 @@
 class CMD_History():
     def __init__(self, size):
         self.buf_size = size
-        self.buf_size = [""] * self.buf_size
+        self.buf = [""] * self.buf_size
         self.w_ptr = 0
         self.r_ptr = 0
 
     def add_item(self, item):
-        pass
+        self.w_ptr += 1
+        if self.w_ptr >= self.buf_size:
+            self.w_ptr = 0
+        self.buf[self.w_ptr] = item
+        self.r_ptr = self.w_ptr
 
     def read_forward(self):
         return ""
     
     def read_backward(self):
-        return ""
+        r = ""
+        if self.r_ptr < 0:
+            self.r_ptr = 0
+        r = self.buf[self.r_ptr]
+        self.r_ptr -= 1
+        return r
 
     def reset_read_pos(self):
         pass
