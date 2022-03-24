f523f9d822826f534bd58be9cd19c124058ba560
Benni <>
2022-03-24 14:06:28 +0100

    methode read_bachward hat den test bestanden

--- src/cmd_history.py
+++ src/cmd_history.py
@@ -6,25 +6,26 @@ class CMD_History():
         self.r_ptr = 0
 
     def add_item(self, item):
+        self.reset_read_pos()
+        self.buf[self.w_ptr] = item
         self.w_ptr += 1
         if self.w_ptr >= self.buf_size:
             self.w_ptr = 0
-        self.buf[self.w_ptr] = item
-        self.r_ptr = self.w_ptr
+        print(f"add item> {self.buf}")
 
     def read_forward(self):
         return ""
     
     def read_backward(self):
-        r = ""
-        if self.r_ptr < 0:
-            self.r_ptr = 0
         r = self.buf[self.r_ptr]
         self.r_ptr -= 1
+        if self.r_ptr < 0:
+            self.r_ptr = self.buf_size - 1
+        print(f"read backward> {r}")
         return r
 
     def reset_read_pos(self):
-        pass
+        self.r_ptr = self.w_ptr
 
     def clear(self):
-        pass
+        self.buf = [""] * self.buf_size
