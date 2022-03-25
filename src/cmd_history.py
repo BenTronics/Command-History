ff0668f5ac62d1a9941fa4bdddb305d03d6a2595
Benni <>
2022-03-25 20:46:03 +0100

    grob fertig

--- src/cmd_history.py
+++ src/cmd_history.py
@@ -6,15 +6,22 @@ class CMD_History():
         self.r_ptr = 0
 
     def add_item(self, item):
-        self.reset_read_pos()
+        if item == self.buf[self.w_ptr-1]:
+            return
         self.buf[self.w_ptr] = item
+        self.r_ptr = self.w_ptr
         self.w_ptr += 1
         if self.w_ptr >= self.buf_size:
             self.w_ptr = 0
         print(f"add item> {self.buf}")
 
     def read_forward(self):
-        return ""
+        r = self.buf[self.r_ptr]
+        self.r_ptr += 1
+        if self.r_ptr >= self.buf_size:
+            self.r_ptr = 0
+        print(f"read forward> {r}")
+        return r
     
     def read_backward(self):
         r = self.buf[self.r_ptr]
@@ -25,7 +32,11 @@ class CMD_History():
         return r
 
     def reset_read_pos(self):
-        self.r_ptr = self.w_ptr
+        self.r_ptr = self.w_ptr - 1
+        if self.r_ptr < 0:
+            self.r_ptr = 0
 
     def clear(self):
         self.buf = [""] * self.buf_size
+        self.w_ptr = 0
+        self.r_ptr = 0
--- test/test.py
+++ test/test.py
@@ -1,4 +1,8 @@
+from distutils.log import debug
 import unittest
+
+import logging
+logging.basicConfig(filename="test.log", filemode="w", level=logging.DEBUG, format="[{levelname}] {message}", style="{")
 
 import sys
 sys.path.append("..")
@@ -10,68 +14,89 @@ cmd_hist = cmd_history.CMD_History(buf_size)
 
 class Test(unittest.TestCase):
     def test_empty_read(self):
-        super().assertEqual(cmd_hist.read_forward(), "")
+        logging.info("test_empty_read")
+        r = cmd_hist.read_forward()
+        logging.debug(f"return value: {r}")
+        self.assertEqual(r, "")
     
     def test_add_item(self):
-        tmp_list_read = ["-"] * buf_size
-        tmp_list_write = ["-"] * buf_size
+        logging.info("test_add_item")
+        tmp_list_write = ["0", "1", "2", "3", "4"]
         cmd_hist.clear()
         # add items until the buffer is full
-        for i in range(buf_size):
-            cmd_hist.add_item(str(i))
-            tmp_list_write[i] = str(i)
-        for i in range(buf_size, 0):
-            tmp_list_read[i] = cmd_hist.read_backward()
-        super().assertEqual(tmp_list_write, tmp_list_read)
+        for elem in tmp_list_write:
+            cmd_hist.add_item(elem)
+            logging.debug(f"write item: {elem}, buffer: {cmd_hist.buf}")
+        self.assertEqual(tmp_list_write, cmd_hist.buf)
     
-    def test_add_item_buffer_overrun(self):
-        tmp_list_read = ["-"] * buf_size
-        tmp_list_write = ["-"] * buf_size
+    def test_buffer_overflow(self):
+        logging.info("test_buffer_overflow")
+        tmp_list_write = ["0", "1", "2", "3", "4", "5", "6"]
+        exspection = ["5", "6", "2", "3", "4"]
         cmd_hist.clear()
         # add more items than the buffer can hold
-        for i in range(buf_size // 2):
-           tmp_list_write[i] = str(i)
-           cmd_hist.add_item(str(i))
-        for i in range(buf_size // 2, 0):
-            tmp_list_read[i] = cmd_hist.read_backward()
-        super().assertEqual(tmp_list_write, tmp_list_read) 
+        for elem in tmp_list_write:
+           cmd_hist.add_item(elem)
+           logging.debug(f"write item: {elem}, buffer: {cmd_hist.buf}")
+        self.assertEqual(cmd_hist.buf, exspection) 
 
     def test_read_backwards(self):
-        tmp_list_read = []
-        tmp_list_write = []
+        logging.info("test_read_backwards")
+        tmp_list_read = ["-"] * buf_size
+        exspection = ["6", "5", "4", "3", "2"]
         cmd_hist.clear()
+        # add more items than the buffer can hold
+        for i in range(buf_size + 2):
+           cmd_hist.add_item(str(i))
+           logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
         for i in range(buf_size):
-            cmd_hist.add_item(str(i))
-            tmp_list_write.append(str(i))
-        # read backwards
-        for i in range(buf_size):
-            tmp_list_read.append(cmd_hist.read_backward())
-        super().assertEqual(tmp_list_read, tmp_list_write)
+            logging.debug(f"r_pointer pos: {cmd_hist.r_ptr}")
+            tmp_list_read[i] = cmd_hist.read_backward()
+            logging.debug(f"read item: {tmp_list_read[i]}")
+        self.assertEqual(exspection, tmp_list_read)
         
     def test_read_forward(self):
-        tmp_list_read = []
-        tmp_list_write = []
+        logging.info("test_read_forward")
+        tmp_list_read = ["-"] * buf_size
+        exspection = ["6", "2", "3", "4", "5"]
+        empty_read = ""
         cmd_hist.clear()
-        # read forward
-        tmp_list_read = []
-        tmp_list_write.reverse()
+        # add more items than the buffer can hold
+        for i in range(buf_size + 2):
+           cmd_hist.add_item(str(i))
+           logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
+        # read all elements backwards
         for i in range(buf_size):
-            tmp_list_read.append(cmd_hist.read_backward())
-        super().assertEqual(tmp_list_read, tmp_list_write)
+            cmd_hist.read_backward()
+        for i in range(buf_size):
+            tmp_list_read[i] = cmd_hist.read_forward()
+            logging.debug(f"read item: {tmp_list_read[i]}")
+        self.assertEqual(exspection, tmp_list_read)
          
     def test_reset_pos(self):
-        tmp_list_read = []
-        tmp_list_write = []
         cmd_hist.clear()
-        # add items until the buffer is full
+        # add items until the buffer overrun
+        for i in range(buf_size+2):
+            cmd_hist.add_item(str(i))
+        # change r_ptr position
+        for i in range(buf_size):
+            cmd_hist.read_backward()
+        cmd_hist.reset_read_pos()
+        self.assertEqual(cmd_hist.read_backward(), "6")
+        cmd_hist.reset_read_pos()
+        self.assertEqual(cmd_hist.read_forward(), "6")
+
+    def test_two_times_item(self):
+        logging.info("test_two_times_item")
+        exspection = ["0", "1", "2", "3", "4"]
+        cmd_hist.clear()
         for i in range(buf_size):
             cmd_hist.add_item(str(i))
-            tmp_list_write.append(str(i))
-        for i in range(buf_size // 2, 0):
-            tmp_list_read.append(cmd_hist.read_backward())
-        cmd_hist.reset_read_pos()
-        super().assertEqual(cmd_hist.read_backward(), "")
-        super().assertEqual(cmd_hist.read_forward(), "")
+            logging.debug(f"writr item: {str(i)}, buffer: {cmd_hist.buf}")
+            cmd_hist.add_item(str(i))
+            logging.debug(f"writr item: {str(i)}, buffer: {cmd_hist.buf}")
+        self.assertEqual(cmd_hist.buf, exspection)
+
 
 if __name__ == "__main__":
     unittest.main()
