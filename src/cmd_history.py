117cbaa8e266302ffc50cc27c070fe32e47ffa0d
Benni <>
2022-03-27 18:15:22 +0200

    test

--- src/cmd_history.py
+++ src/cmd_history.py
@@ -4,39 +4,54 @@ class CMD_History():
         self.buf = [""] * self.buf_size
         self.w_ptr = 0
         self.r_ptr = 0
+        self.num_of_read_back = 0
+        self.num_of_read_forward = 0
+        self.num_of_items = 0
+        self.state = "idle"
 
     def add_item(self, item):
         if item == self.buf[self.w_ptr-1]:
             return
+        self.num_of_items += 1
+        if self.num_of_items >= self.buf_size:
+            self.num_of_items = self.buf_size
         self.buf[self.w_ptr] = item
         self.r_ptr = self.w_ptr
         self.w_ptr += 1
         if self.w_ptr >= self.buf_size:
             self.w_ptr = 0
-        print(f"add item> {self.buf}")
 
     def read_forward(self):
+        if self.state == "backward":
+            self.r_ptr += 2
+            if self.r_ptr >= self.buf_size:
+                self.r_ptr = 0
         r = self.buf[self.r_ptr]
-        self.r_ptr += 1
-        if self.r_ptr >= self.buf_size:
-            self.r_ptr = 0
-        print(f"read forward> {r}")
+        if self.num_of_read_back > self.num_of_read_forward:
+            self.num_of_read_forward += 1
+        if self.num_of_read_back > self.num_of_read_forward:
+            self.r_ptr += 1
+            if self.r_ptr >= self.buf_size:
+                self.r_ptr = 0
+        self.state = "forward"
         return r
     
     def read_backward(self):
         r = self.buf[self.r_ptr]
-        self.r_ptr -= 1
-        if self.r_ptr < 0:
-            self.r_ptr = self.buf_size - 1
-        print(f"read backward> {r}")
+        if self.num_of_read_back < self.num_of_items:
+            self.num_of_read_back += 1
+        if self.num_of_read_back < self.num_of_items:
+            self.r_ptr -= 1
+            if self.r_ptr < 0:
+                self.r_ptr = self.buf_size - 1
+        self.state = "backward"
         return r
-
-    def reset_read_pos(self):
-        self.r_ptr = self.w_ptr - 1
-        if self.r_ptr < 0:
-            self.r_ptr = 0
 
     def clear(self):
         self.buf = [""] * self.buf_size
         self.w_ptr = 0
         self.r_ptr = 0
+        self.num_of_items = 0
+        self.num_of_read_back = 0
+        self.num_of_read_forward = 0
+        self.state = "idle"
--- test/test.py
+++ test/test.py
@@ -15,6 +15,7 @@ cmd_hist = cmd_history.CMD_History(buf_size)
 class Test(unittest.TestCase):
     def test_empty_read(self):
         logging.info("test_empty_read")
+        cmd_hist.clear()
         r = cmd_hist.read_forward()
         logging.debug(f"return value: {r}")
         self.assertEqual(r, "")
@@ -58,7 +59,7 @@ class Test(unittest.TestCase):
     def test_read_forward(self):
         logging.info("test_read_forward")
         tmp_list_read = ["-"] * buf_size
-        exspection = ["6", "2", "3", "4", "5"]
+        exspection = ["2", "3", "4", "5", "6"]
         empty_read = ""
         cmd_hist.clear()
         # add more items than the buffer can hold
@@ -72,30 +73,90 @@ class Test(unittest.TestCase):
             tmp_list_read[i] = cmd_hist.read_forward()
             logging.debug(f"read item: {tmp_list_read[i]}")
         self.assertEqual(exspection, tmp_list_read)
-         
-    def test_reset_pos(self):
-        cmd_hist.clear()
-        # add items until the buffer overrun
-        for i in range(buf_size+2):
-            cmd_hist.add_item(str(i))
-        # change r_ptr position
-        for i in range(buf_size):
-            cmd_hist.read_backward()
-        cmd_hist.reset_read_pos()
-        self.assertEqual(cmd_hist.read_backward(), "6")
-        cmd_hist.reset_read_pos()
-        self.assertEqual(cmd_hist.read_forward(), "6")
 
     def test_two_times_item(self):
         logging.info("test_two_times_item")
-        exspection = ["0", "1", "2", "3", "4"]
+        exspection = ["5", "6", "2", "3", "4"]
         cmd_hist.clear()
-        for i in range(buf_size):
+        for i in range(buf_size + 2):
             cmd_hist.add_item(str(i))
-            logging.debug(f"writr item: {str(i)}, buffer: {cmd_hist.buf}")
+            logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
             cmd_hist.add_item(str(i))
-            logging.debug(f"writr item: {str(i)}, buffer: {cmd_hist.buf}")
+            logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
         self.assertEqual(cmd_hist.buf, exspection)
+    
+    def test_block_backward(self):
+        logging.info("test_block_backward")
+        cmd_hist.clear()
+        r = ""
+        # fill buffer
+        for i in range(2):
+            cmd_hist.add_item(str(i))
+            logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
+            logging.debug(f"number of items in buf: {cmd_hist.num_of_items}")
+        # read buffer
+        for i in range(2):
+            r = cmd_hist.read_backward()
+            logging.debug(f"read item: {r}")
+            logging.debug(f"num of readback: {cmd_hist.num_of_read_back}")
+        logging.debug(f"r_ptr {cmd_hist.r_ptr}")
+        r = cmd_hist.read_backward()
+        logging.debug(f"read item: {r}")
+        logging.debug(f"num of readback: {cmd_hist.num_of_read_back}")
+        self.assertEqual(r, "0")
+    
+    def test_block_forward(self):
+        logging.info("test_block_forward")
+        cmd_hist.clear()
+        r = ""
+        # fill buffer
+        for i in range(3):
+            cmd_hist.add_item(str(i))
+            logging.debug(f"write item: {str(i)}, buffer: {cmd_hist.buf}")
+            logging.debug(f"number of items in buf: {cmd_hist.num_of_items}")
+        # read buffer backward
+        for i in range(3):
+            r = cmd_hist.read_backward()
+            logging.debug(f"read (backward) item: {r}")
+            logging.debug(f"num of readback: {cmd_hist.num_of_read_back}")
+        # read forward
+        for i in range(3):
+            r = cmd_hist.read_forward()
+            logging.debug(f"read (forward) item: {r}")
+        logging.debug(f"r_ptr {cmd_hist.r_ptr}")
+        r = cmd_hist.read_forward()
+        logging.debug(f"read (forward) item: {r}")
+        logging.debug(f"num of readback: {cmd_hist.num_of_read_back}")
+        self.assertEqual(r, "2")
+    
+    def test_use_case(self):
+        logging.info("test_use_case")
+        cmd_hist.clear()
+        r = ""
+        # try to read empty buffer
+        r = cmd_hist.read_backward()
+        logging.debug(f"arrow up: {r}")
+        self.assertEqual(r, "")
+        r = cmd_hist.read_forward()
+        logging.debug(f"arrow down: {r}")
+        self.assertEqual(r, "")
+        # fill the buffer with 3 elements
+        for i in range(3):
+            cmd_hist.add_item("CMD" + str(i))
+        logging.debug(f"buf: {cmd_hist.buf}")
+        # read 2 elements backwards and 1 forward
+        r = cmd_hist.read_backward()
+        logging.debug(f"arrow up: {r}")
+        r = cmd_hist.read_backward()
+        logging.debug(f"arrow up: {r}")
+        r = cmd_hist.read_forward()
+        logging.debug(f"arrow down: {r}")
+        r = cmd_hist.read_forward()
+        logging.debug(f"arrow down: {r}")
+        # add 2 items
+        # read 2 elements backwards and 1 forward
+        # add 3 items
+        # read 3 elements backwards and 4 forward
 
 
 if __name__ == "__main__":
